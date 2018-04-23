0e0f856886e592f365e559a32d36c264ac92d56b
khalil <khalil.ov@yandex.ru>
2018-04-23 21:15:53 +0300

    add limit to lines in textarea

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,32 @@
 function game() {
-    $('#next').addClass('disabled');
     var bgStyle =$("#background").attr("style").replace(/\s/g, '');
+    $('#next').addClass('disabled');
+
     $("#code").on("keyup", function (e) {
+        var k = e.keyCode || e.which;
         var code = $("#code").val().replace(/\s/g, '');
+
         $("#pond").attr("style", code);
-        $("#editor").removeClass("animate");
+        $("#editor").removeClass("animate-code");
         if(code === bgStyle) {
             $('#next').removeClass('disabled');
         } else {
             $('#next').addClass('disabled');
         }
-        var k = e.keyCode || e.which;
+
         if(k === 13 && code === bgStyle) {
-            $('#next').trigger('click');
-
+            document.location = $("#next").attr('href');
         } else if(k === 13 && code !== bgStyle)  {
-            $("#editor").addClass('animate');
+            $("#editor").addClass('animate-code');
         }
+    });
+    $('#code').keydown(function(e) {
+        var lines = 1;
+        var newLines = $(this).val().split("\n").length;
 
+        if(e.keyCode === 13 && newLines >= lines) {
+            return false;
+        }
     });
 }
 $(document).ready(function () {
